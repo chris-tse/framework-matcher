a13976d6ae000b015443e9c5c9adccf330c45ea4
Christopher Tse <chris@chris-tse.com>
2020-07-11 11:24:26 -0500

    Fix react logo and export all from index

---
+++ src/logos/index.ts
@@ -0,0 +1,11 @@
+import angular from './angular.svg'
+import react from './react.svg'
+import vue from './vue.svg'
+
+const logos: Record<string, string> = {
+    angular,
+    react,
+    vue,
+}
+
+export default logos
