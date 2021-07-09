a0cc311f6e2284c2e5d66bde92f29dd5254072b6
Chris Tse <chris@chris-tse.com>
2021-07-09 17:30:52 -0500

    Fix incorrect rendering on Mac OS Chrome
    The framework logo would briefly show during the flip animation but disappear once it is done. Moving the background color to each "side" div instead of the parent card seems to fix it: https://stackoverflow.com/questions/37914624/flip-animation-is-not-working-in-chrome-on-mac-os-x

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,19 +16,19 @@ function Card({ text, flipped, handleCardClick, cardId }: CardProps) {
     return (
         <div
             onClick={handleClick}
-            className={`bg-white border border-gray-200 rounded-md shadow-md hover:shadow-lg cursor-pointer text-center relative mx-4 my-4 h-full w-full inline-flex justify-center items-center transition duration-500 ${
+            className={`border border-gray-200 rounded-md shadow-md hover:shadow-lg cursor-pointer text-center relative mx-4 my-4 h-full w-full inline-flex justify-center items-center transition duration-500 ${
                 flipped ? 'is-flipped' : ''
             }`}
             style={{ transformStyle: 'preserve-3d' }}
         >
             <div
-                className="absolute w-full h-full flex flex-col justify-center backface-hidden"
+                className="bg-white absolute w-full h-full flex flex-col justify-center backface-hidden"
                 style={{ transformStyle: 'preserve-3d' }}
             >
                 ?
             </div>
             <div
-                className="absolute w-full h-full flex flex-col justify-center p-4 backface-hidden"
+                className="bg-white absolute w-full h-full flex flex-col justify-center p-4 backface-hidden"
                 style={{ transform: 'rotateY(180deg)' }}
             >
                 <img src={logos[text.toLowerCase()]} alt={`${text} logo`} />
